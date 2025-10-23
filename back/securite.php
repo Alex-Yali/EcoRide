9c228b00153bdb3d83472b4a21b90bb0eb14d1d1
Alex.Y <alexandre.yalicheff@gmail.com>
2025-10-23 19:07:27 +0200

    Suppression fichier non utilisé

--- back/securite.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// --- 1. Protection CSRF ---
-if (!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']) {
-    exit("Tentative d’accès non autorisée !");
-}
-
