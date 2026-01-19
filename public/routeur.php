85b2e1b7e1408e6f69f345d630b1a8648d739f46
Alex.Y <alexandre.yalicheff@gmail.com>
2026-01-20 00:41:12 +0100

    patch 3

---
+++ public/routeur.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+require_once __DIR__ . '/../src/service/db.php';
+require_once __DIR__ . '/../src/service/csrf.php';
+require_once __DIR__ . '/../src/repository/gestionAvis.php';
